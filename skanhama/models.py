f76397a57372a0515d440bebc1e4006448f27f72
Rickard Lundqvist <rickard.jj.lundqvist@gmail.com>
2021-10-27 01:19:04 +0200

    Before CSS styling change

---
+++ skanhama/models.py
@@ -0,0 +1,21 @@
+from skanhama import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(23), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_on = db.Column(db.DateTime, nullable=True)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    def __repr__(self):
+        return f"User ('{self.id}', '{self.username}', '{self.email}', '{self.confirmed_on}, 'Admin={self.admin}')"
--- skanhama/routes.py
+++ skanhama/routes.py
@@ -1,5 +1,9 @@
-from flask import render_template, url_for, flash, redirect
-from skanhama import app
+from datetime import datetime
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from skanhama import app, db
+from skanhama.models import User
 from skanhama.forms import RegistrationForm, LoginForm
 
 
@@ -11,8 +15,15 @@ def home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, "sha256")
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password,
+                    registered_on=datetime.now(), confirmed=False, admin=False)
+        db.session.add(user)
+        db.session.commit()
         flash(f"An email has been sent to the registered email for user {form.username.data}. Please "
               f"follow the activation link in the email to activate your account.", "success")
         return redirect(url_for("home"))
@@ -21,14 +32,30 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == "reddit@blog.com" and form.password.data == "reddit":
-            flash("You have been successfully logged in.", "success")
-            return redirect(url_for("home"))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
-            flash("Login unsuccessful. Please check username and password.", "fail")
+            flash("Login unsuccessful. Please check email and password.", "fail")
     return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
 
 
 @app.route("/browse")
